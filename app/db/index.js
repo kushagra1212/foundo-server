f36a3b59601885811974b7b7f52fcf0d709440e3
kushaga1212 <kushagrarathore002@gmail.com>
2023-01-16 01:52:03 +0530

    feat: add Message

--- app/db/index.js
+++ app/db/index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-//const db = require('../config/db');
+//const db = require('../config/db'); // Production
 const db = require('../config/db-dev');
 const pool = mysql.createPool(db.config);
 module.exports = pool.promise();
---
+++ app/models/Message.js
@@ -0,0 +1,48 @@
+const promisePool = require('../db');
+
+class Message {
+  constructor({
+    senderId,
+    receiverId,
+    message,
+    title,
+    isPhoneNoShared,
+    isFound,
+  }) {
+    this.senderId = senderId;
+    this.receiverId = receiverId;
+    this.message = message;
+
+    this.title = title;
+    this.isPhoneNoShared = isPhoneNoShared;
+    this.isFound = isFound;
+  }
+  save() {
+    let sql = `INSERT INTO messages(senderId,receiverId,message,title,isPhoneNoShared,isFound) VALUES(?,?,?,?,?,?)`;
+    return promisePool.execute(sql, [
+      this.senderId,
+      this.receiverId,
+      this.message,
+      this.title,
+      this.isPhoneNoShared,
+      this.isFound,
+    ]);
+  }
+  static getContactList({ userId, limit, offset }) {
+    let sql = `select m1.senderId,u1.firstName,u1.lastName,m1.messagesCount from (select distinct(senderId),count(*) as messagesCount from messages where receiverId=? group by senderId) as m1 inner join (select id,firstName,lastName from users) as u1 on m1.senderId=u1.id limit ? offset ?`;
+    return promisePool.execute(sql, [userId, limit, offset]);
+  }
+  static getMessages({ senderId, receiverId, limit, offset }) {
+    let sql = `SELECT * FROM messages WHERE (senderId=? AND receiverId=?) OR (senderId=? AND receiverId=?) ORDER BY createdAt DESC LIMIT ? OFFSET ?`;
+    return promisePool.execute(sql, [
+      senderId,
+      receiverId,
+      receiverId,
+      senderId,
+      limit,
+      offset,
+    ]);
+  }
+}
+
+module.exports = Message;
