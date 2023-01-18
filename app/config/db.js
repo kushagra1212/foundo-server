0de8c89c8f2a9ce04a9e8424b63606bf503ed484
kushaga1212 <kushagrarathore002@gmail.com>
2023-01-18 22:14:44 +0530

    fix: changed the .env Names for AWS configs

--- app/config/db.js
+++ app/config/db.js
@@ -1,9 +1,9 @@
 const config = {
-  host: process.env.AWS_DB_HOST,
-  user: process.env.AWS_DB_USER,
-  password: process.env.AWS_DB_PASSWORD,
-  port: process.env.AWS_DB_PORT,
-  database: process.env.DATABASE,
+  host: process.env.FAWS_DB_HOST,
+  user: process.env.FAWS_DB_USER,
+  password: process.env.FAWS_DB_PASSWORD,
+  port: process.env.FAWS_DB_PORT,
+  database: process.env.FDATABASE,
 };
 
 module.exports = { config };
