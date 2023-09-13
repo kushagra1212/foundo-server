aabdeaa3c8c54fec4085e90fb93c8cb7434104b2
kushaga1212 <kushagrarathore002@gmail.com>
2023-09-14 03:28:55 +0530

    (feat): Add Recommandation

--- app/db/index.js
+++ app/db/index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-const db = require('../config/db'); // Production
-//const db = require('../config/db-dev');
+//const db = require('../config/db'); // Production
+const db = require('../config/db-dev'); // Devlopment
 const pool = mysql.createPool(db.config);
 module.exports = pool.promise();
