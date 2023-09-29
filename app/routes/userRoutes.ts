386d2928523d8a7588d4fc40131582de73c5cbd9
Kushagra <kushagrarathore002@gmail.com>
2023-09-29 16:54:27 +0530

    refactor(user): Changed the APIs

--- app/routes/userRoutes.ts
+++ app/routes/userRoutes.ts
@@ -4,6 +4,15 @@ import { auth } from '../middleware/auth';
 
 const router = express.Router();
 
+/* Send OTP */
+router.get('/send/otp/:id', auth, userControllers.sendOtp);
+
+/* Reset OTP */
+router.get('/reset/otp/:id', auth, userControllers.resetOtp);
+
+/* Verify OTP */
+router.get('/verify/otp/:id/:otp', auth, userControllers.verifyOtp);
+
 /* User Signup */
 router.post('/signup', userControllers.signupUser);
 
@@ -11,7 +20,7 @@ router.post('/signup', userControllers.signupUser);
 router.post('/signin', userControllers.signinUser);
 
 /* Get All Users */
-router.get('', userControllers.getAllUsers);
+router.get('/:limit/:offset', auth, userControllers.getAllUsers);
 
 /* Get User By Id */
 router.get('/:id', auth, userControllers.getUserById);
@@ -21,15 +30,6 @@ router.patch('/:id', auth, userControllers.updateUserById);
 
 /* Delete User By Id */
 router.delete('/:id', auth, userControllers.deleteUserById);
-
-/* Send OTP */
-router.get('/send/otp/:id', auth, userControllers.sendOtp);
-
-/* Verify OTP */
-router.get('/verify/otp/:id/:otp', auth, userControllers.verifyOtp);
-
-/* Reset OTP */
-router.get('/reset/otp/:id', auth, userControllers.resetOtp);
 
 // delete request Delete by Id
 router.delete('/', userControllers.deleteUserById);
--- dist/app/config/db.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DATABASE,
-};
-module.exports = { config };
-//# sourceMappingURL=db.js.map
