99f7090866d9ae6a75bb83d984e4f0fffbf053e7
Kushagra <kushagrarathore002@gmail.com>
2023-10-01 12:06:22 +0530

    (fix): the server handle open

--- server.ts
+++ server.ts
@@ -41,7 +41,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true,  limit: '50mb' }));
 
 // starting the app
-app.listen(PORT, () => {
+export const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
